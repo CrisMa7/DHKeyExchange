178106166c9f8f3edc8fecf5d26cd0b161c5b5e9
CrisMa7 <mayf@cryptape.com>
2018-06-12 14:27:40 +0800

    commit

---
+++ src/DHTest.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import org.apache.commons.codec.binary.Base64;
+public class DHTest {
+	//Alice 公私钥 对称密钥
+	private static byte[] publicKeyAlice;
+	private static byte[] privateKeyAlice;
+	private static byte[] keyAlice;
+	//Bob 公私钥 对称密钥
+	private static byte[] publicKeyBob;
+	private static byte[] privateKeyBob;
+	private static byte[] keyBob;
+	
+	 /** 
+     * 初始化密钥 
+     * @throws Exception 
+     */
+	public static final void initKey() throws Exception{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
